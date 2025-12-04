27ec3e3288e11cdc956806f744408653e074f5fd
x0backslash0x <patrick.lanove@student.ap.be>
2025-12-04 14:55:40 +0100

    opdracht5  stap 5a1

---
+++ opdracht5/server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Hierdoor kan de server je front-end bestand tonen (index.html)
+indexfile = path.join(__dirname, 'index.html')
+app.get("/", (req, res) => {
+    res.status(200).sendFile(indexfile)
+});
+
+// Start server op poort 3000
+const port = 3000;
+app.listen(port, () => console.log(`Server draait op http://localhost:${port}`));
